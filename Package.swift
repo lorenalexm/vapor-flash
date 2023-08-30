0f2f3e566276543a4dc0d0d1c167bef69bd52945
Alex Loren <lorenalexm@gmail.com>
2023-08-29 18:38:31 -0700

    Updated dependencies version requirements

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .library(name: "Flash", targets: ["Flash"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.0.0")),
     ],
     targets: [
         .target(name: "Flash", dependencies: [
